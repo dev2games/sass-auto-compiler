cf3c69eca3bbcc7747d8fdbf37345dbcb52ed185
Nelson Sanchez <nelson@dev2games.com>
2018-07-19 01:46:59 -0400

    Added create directory

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
-#Python is awesome
+# Python is awesome
+#@author Nelson Sanchez
 
-#Arguments
-directory = ""
-output = ""
-optimization = ""
+import os
+import sys
+import threading
 
-while not directory:
-    directory = input("Please enter your sass directory folder: [/home/dev2games/Desktop/project/sass/]")
+# Arguments
+args = {
+    "directory": "",
+    "output": "",
+    "optimization": ""
+}
 
-while not output:
-    output = input("Please enter the output directory for the style.css: [/home/dev2games/Desktop/project/css/]")
 
-while optimization != 'n' and optimization != 'y':
-    optimization = input("(Optimization) Will your sass folder have subdirectories? [y/n]: ").lower()
+# Runs the code continuously every 2 seconds
+def init():
+    threading.Timer(1.0, init).start()
+    # Do not run if the initial arguments are undefined
+    print("Hello World")
 
+def createDirectory(dir):
+    # Checks if the directory exists
+    if not os.path.isdir(dir) or not os.path.exists(dir):
+        createDir = ""
+        while not createDir:
+            createDir = input("Directory does not exist. Create it now? [y/n]").lower()
+    if createDir == "y":
+        print("Directory has been created!")
+        os.mkdir(dir)
+    else:
+        print("Exiting program, directory does not exist at: " + dir)
+        sys.exit()
+
+
+
+while not args["directory"]:
+    args["directory"] = input("Please enter your sass directory folder: [/home/dev2games/Desktop/project/sass/]")
+    createDirectory(args["directory"])
+
+while not args["output"]:
+    args["output"] = input("Please enter the output directory for the style.css: [/home/dev2games/Desktop/project/css/]")
+
+while args["optimization"] != 'n' and args["optimization"] != 'y':
+    args["optimization"] = input("(Optimization) Will your sass folder have subdirectories? [y/n]: ").lower()
 
 print("Perform some function")
 
+init()
